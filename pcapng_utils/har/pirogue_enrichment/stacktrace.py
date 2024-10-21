5002aef7aa1d2594c7de57be923e1ce79b236f76
Etienne Maheux <etienne.maheux@peren.gouv.fr>
2024-10-21 19:16:34 +0200

    Efficient socket trace <-> network traffic pairing

--- pcapng_utils/har/pirogue_enrichment/stacktrace.py
+++ pcapng_utils/har/pirogue_enrichment/stacktrace.py
@@ -3,42 +3,65 @@
 
 import logging
 from pathlib import Path
-from collections.abc import Set
-from typing import Literal, ClassVar, Any
+from operator import itemgetter
+from collections import defaultdict
+from collections.abc import Mapping
+from typing import TypedDict, Literal, ClassVar, Any
 
 import communityid
+from sortedcontainers import SortedKeyList
 
-from . import HarEnrichment
-from .utils import prefix_string_camel_case, clean_prefixed_ip_address
+from .base import HarEnrichment
+from .types import CommunityID, Timestamp, FlowDirection
+from .utils import keys_to_camel_case, clean_prefixed_ip_address
 
 logger = logging.getLogger('enrichment')
+
+
+class SocketTraceData(TypedDict):  # and other stuff that is not
+    stack: list[dict]
+    socketEventType: str
+    localIp: str
+    localPort: int
+    destIp: str
+    destPort: int
+    socketType: Literal['tcp', 'udp', 'tcp6', 'udp6']
+    communityId: CommunityID
+
+
+class SocketTrace(TypedDict):
+    process: str
+    timestamp: Timestamp  # seconds
+    data: SocketTraceData
+
+
+def empty_time_sorted_list_of_stack_traces():
+    return SortedKeyList(key=itemgetter('timestamp'))
 
 
 class Stacktrace(HarEnrichment):
 
     ID: ClassVar = 'stacktrace'
 
+    KEYS_PREFIX: ClassVar = ''
+    COMMUNITY_ID: ClassVar = communityid.CommunityID()
+    MAX_DELTA_S: ClassVar[float] = 1.0
+    """maximum delay (in seconds) tolerated between socket operation & network traffic for matching them"""
+
     def __init__(self, har_data: dict, input_data_file: Path) -> None:
         super().__init__(har_data, input_data_file)
-        self.socket_traces: list[dict] = []
+        self.socket_traces_map: Mapping[tuple[CommunityID, FlowDirection], SortedKeyList] = defaultdict(empty_time_sorted_list_of_stack_traces)
+        self.paired_socket_traces: dict[tuple[CommunityID, FlowDirection, int], float] = {}
 
         if self.can_enrich:
             # Preprocess the socket traces: remove unnecessary fields and prefix keys
-            self.socket_traces = self._preprocess_socket_traces(self.input_data, prefix='')  # type: ignore
+            self._preprocess_socket_traces()
         else:
             logger.warning('HAR enrichment with stacktrace information cannot be performed, skip.')
 
-    @staticmethod
-    def _can_enrich_directed_entry(har_directed_entry: dict[str, Any]) -> bool:
-        """Check if the given HAR entry can be enriched with stacktrace information"""
-        return bool(har_directed_entry.get('_timestamp'))
-
-    @staticmethod
-    def _attach_community_id_to_stacktrace(socket_trace_data: dict) -> None:
+    @classmethod
+    def _attach_community_id_to_stacktrace(cls, socket_trace_data: dict) -> None:
         """Compute and append in-place the Community ID to the given stacktrace"""
-        if 'communityId' in socket_trace_data:  # already done... (TODO: refact)
-            return
-        cid = communityid.CommunityID()
         src_ip = clean_prefixed_ip_address(socket_trace_data['localIp'])
         src_port = socket_trace_data['localPort']
         dst_ip = clean_prefixed_ip_address(socket_trace_data['destIp'])
@@ -49,46 +72,75 @@ class Stacktrace(HarEnrichment):
         else:
             tpl = communityid.FlowTuple.make_udp(src_ip, dst_ip, src_port, dst_port)
         # Attach the Community ID
-        socket_trace_data['communityId'] = cid.calc(tpl)
+        socket_trace_data['communityId'] = cls.COMMUNITY_ID.calc(tpl)
 
-    def _find_stacktrace(self, community_id: str, timestamp: float, socket_operations: Set[str]) -> dict:
-        """ Find the stacktrace with the closest timestamp to the given one matching the community ID """
-        # TODO refact: add once all community IDs + convert as a mapping[cid, sorted_list[(timestamp, socket_data)]]
-        best_guess: dict = {}
-        min_time = 2. ** 31 - 1
-        # The objective is to find the stacktrace by minimizing the time difference between the HAR entry and
-        # the stacktrace timestamp
-        for socket_trace in self.socket_traces:
-            socket_data: dict = socket_trace['data']
-            # No stacktrace data, skip
-            if 'stack' not in socket_data:
+    @classmethod
+    def _get_clean_stacktrace(cls, stacktrace: dict) -> SocketTrace:
+        """
+        Get a clean stacktrace object by removing unnecessary fields,
+        renaming keys in camel case (with optional prefix) and ensuring the
+        timestamp is in seconds (instead of milliseconds).
+
+        Side-effects free.
+        """
+        clean_trace = keys_to_camel_case({
+            'timestamp': stacktrace['timestamp'] / 1000.,
+            'process': stacktrace['process'],
+            'data': stacktrace['data'],
+        }, prefix=cls.KEYS_PREFIX)
+        cls._attach_community_id_to_stacktrace(clean_trace['data'])
+        return clean_trace  # type: ignore
+
+    def _preprocess_socket_traces(self) -> None:
+        """Create the mapping of stock traces (by community ID + flow direction) to efficiently attach them afterwards."""
+        assert isinstance(self.input_data, list), type(self.input_data)
+        for raw_stack_trace in self.input_data:
+            clean_stack_trace = self._get_clean_stacktrace(raw_stack_trace)
+            socket_type = clean_stack_trace['data']['socketEventType']
+            flow_dir: FlowDirection | None = 'out' if socket_type in {'write', 'sendto'} else 'in' if socket_type in {'read', 'recvfrom'} else None
+            if flow_dir is None:
                 continue
-            self._attach_community_id_to_stacktrace(socket_data)
-            # Check if the community ID and the socket operations match
-            if socket_data['communityId'] == community_id and socket_data['socketEventType'] in socket_operations:
-                trace_timestamp = socket_trace['timestamp']
-                delta = abs(trace_timestamp - timestamp)
-                if delta < min_time:
-                    min_time = delta
-                    best_guess = socket_trace
-        if best_guess:
-            logger.debug(f'Stacktrace found with ∆t={min_time * 1000:.1f}ms, for {community_id} {socket_operations}')
-        else:
-            logger.warning(f'No stacktrace has been found for {community_id}')
-        return best_guess
+            # TODO: check that timestamp != of others?
+            self.socket_traces_map[(clean_stack_trace['data']['communityId'], flow_dir)].add(clean_stack_trace)
 
     @staticmethod
-    def _compact_stack_trace(stack_trace: dict) -> list[str]:
-        """Compact the stacktrace for convenience"""
-        clean_stack = []
-        stack = stack_trace['stack']
-        for call in stack:
-            clazz: str = call['class']
-            if clazz not in clean_stack:
-                clean_stack.append(clazz)
-        return clean_stack
+    def _can_enrich_directed_entry(har_directed_entry: dict[str, Any]) -> bool:
+        """Check if the given HAR entry can be enriched with stacktrace information"""
+        return bool(har_directed_entry.get('_timestamp'))
 
-    def _enrich_directed_entry(self, har_entry: dict[str, Any], community_id: str, direction: Literal['in', 'out']) -> None:
+    def _find_best_stacktrace(self, community_id: CommunityID, direction: FlowDirection, timestamp: Timestamp) -> SocketTrace | None:
+        """ Find the stacktrace with the closest timestamp (in the past) to the given one matching the community ID """
+        matching_traces = self.socket_traces_map.get((community_id, direction))
+        if not matching_traces:
+            logger.warning(f'No stacktrace has been found for {community_id=}, {direction=}')
+            return None
+        closest_stack_trace_ix: int = matching_traces.bisect_key_left(timestamp)
+        if closest_stack_trace_ix == len(matching_traces):
+            logger.warning(f'No socket operation prior to {timestamp=} has been found for {community_id=}, {direction=}')
+            return None
+        closest_match: SocketTrace = matching_traces[closest_stack_trace_ix]  # type: ignore
+        delta_sec = timestamp - closest_match['timestamp']
+        if delta_sec > self.MAX_DELTA_S:
+            logger.warning(f'Closest past socket operation < {timestamp=} for {community_id=}, {direction=} is too old')
+            return None
+        pairing_key = (community_id, direction, closest_stack_trace_ix)
+        already_paired_delta_sec = self.paired_socket_traces.get(pairing_key)
+        if already_paired_delta_sec is not None:
+            raise NotImplementedError(
+                f"TODO: find best OVERALL allocations of stacktraces under ({community_id=}, {direction=}) instead of FIFO? "
+                f"{delta_sec=}, {already_paired_delta_sec=}, {closest_match['timestamp']=}"
+            )
+        self.paired_socket_traces[pairing_key] = delta_sec
+        logger.debug(f'Stacktrace found with ∆t={delta_sec * 1000:.1f}ms, for {community_id=}, {direction=}')
+        return closest_match
+
+    @staticmethod
+    def _compact_stack_trace(stack_trace: SocketTraceData) -> list[str]:
+        """Compact the stacktrace for convenience"""
+        # order of dictionary keys is officially maintained since Python >= 3.7
+        return list({call['class']: 0 for call in stack_trace['stack']})
+
+    def _enrich_directed_entry(self, har_entry: dict[str, Any], community_id: CommunityID, direction: FlowDirection) -> None:
         """Attach the stacktrace to the given HAR directed entry (either request or response), in-place"""
         # Fail first
         if direction not in ('in', 'out'):
@@ -96,50 +148,14 @@ class Stacktrace(HarEnrichment):
         if not self._can_enrich_directed_entry(har_entry):
             return
         # Use read operations on the socket when dealing with a response (in), write operations otherwise
-        socket_operations = {'write', 'sendto'} if direction == 'out' else {'read', 'recvfrom'}
-        timestamp = float(har_entry['_timestamp'])
-        stack_trace = self._find_stacktrace(community_id, timestamp, socket_operations)
-        stack_trace_data = stack_trace.get('data', {})
-        # Attach the stacktrace to the HAR entry if found
-        if stack_trace_data:
-            har_entry['_stacktrace'] = stack_trace_data
-            har_entry['_stacktrace']['compact'] = self._compact_stack_trace(stack_trace_data)
-
-    def _prefix_keys(self, obj: str | dict | list, prefix: str = '_') -> Any:
-        """Prefix all the keys of a dictionary or list of dictionaries recursively"""
-        if isinstance(obj, dict):
-            empty = {}
-            for k, v in obj.items():
-                empty[prefix_string_camel_case(k, prefix)] = self._prefix_keys(v, prefix=prefix)
-        elif isinstance(obj, list):
-            empty = []
-            for k in obj:
-                empty.append(self._prefix_keys(k, prefix=prefix))
-        else:
-            empty = obj
-        return empty
-
-    def _clean_stacktrace_data(self, stacktrace: dict, prefix: str = '') -> dict:
-        """
-        Clean the stacktrace data by removing unnecessary fields, prefixing keys if necessary and ensure the
-        timestamp is in seconds.
-
-        <!> do not run twice!
-        """
-        stacktrace['timestamp'] /= 1000.
-        del stacktrace['type']
-        del stacktrace['data_type']
-        del stacktrace['dump']
-        del stacktrace['pid']
-        # Prefix the keys of the stacktrace data
-        processed_stacktrace = self._prefix_keys(stacktrace, prefix=prefix)
-        return processed_stacktrace
-
-    def _preprocess_socket_traces(self, socket_traces: list[dict], prefix: str = '') -> list[dict]:
-        return [
-            self._clean_stacktrace_data(socket_trace, prefix)
-            for socket_trace in socket_traces
-        ]
+        stack_trace = self._find_best_stacktrace(community_id, direction, Timestamp(har_entry['_timestamp']))
+        if stack_trace:  # Attach the stacktrace to the HAR entry if found
+            har_entry['_stacktrace'] = {
+                'timestamp': stack_trace['timestamp'],
+                'process': stack_trace['process'],
+                **stack_trace['data'],
+                'compact': self._compact_stack_trace(stack_trace['data']),
+            }
 
     def enrich_entry(self, har_entry: dict[str, Any]) -> None:
         """Enrich the HAR data with the stacktraces information"""
